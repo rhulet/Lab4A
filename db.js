af9a00dbc151410b0f458ee6ff3f6645155cbd70
Chance Murray <chance.s.murray@gmail.com>
2019-03-01 08:52:28 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 /*************************************************************************
-Fill out this file with Sequelize database connection information.
+FOR PART 1: Fill out this file with Sequelize database connection information.
 Make sure that you "export" (make externally available) the resources that
 you create in this file. You should need no more than about 4 lines of code
 in this file. You should read the documentation referenced on the lab specs
