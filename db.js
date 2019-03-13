8034a4dabd62344889eb0a4be5de2267e960a858
Ryan Hulet <rhulet@ameritech.edu>
2019-03-13 06:29:08 -0700

    Fixed db in prod

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ will be similar to the database connection file you made for PHP in Lab 3.
 
 let Sequelize = require('sequelize');
 
-let sequelize = new Sequelize('it210b', 'webadmin', 'zxcfrewQ1!', {
+let sequelize = new Sequelize('it210b', 'root', 'zxcfrewQ1!', {
     host: '192.168.90.65',
     dialect: 'mysql'
 })
